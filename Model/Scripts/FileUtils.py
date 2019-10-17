38a449a215970e722a86a2772c959faa920eebe7
Chenyu Xi <xi_chenyu@163.com>
2019-10-16 22:17:10 -0400

    build datasets

---
+++ Model/Scripts/FileUtils.py
@@ -0,0 +1,53 @@
+from Settings import Settings
+import json
+
+
+class FileUtils:
+
+    @staticmethod
+    def save_dict(mapper, file_name):
+        """
+        Save dictionary to json file
+        :param mapper:
+        :param file_name:
+        :return:
+        """
+        path = Settings.data_folder + file_name
+        with open(path, "w") as file:
+            json.dump(mapper, file)
+        return
+
+    @staticmethod
+    def index_sentence(sentence, word_dict):
+        """
+        Convert sentence to index list
+        :param sentence:
+        :param word_dict:
+        :return: list
+        """
+        words = sentence.strip().split()
+        return [word_dict.get(word, 0) for word in words]
+
+    @staticmethod
+    def divide_sentence(words, seq_size):
+        """
+        divide sentence & padding
+        :param words: document
+        :param seq_size: chunk size
+        :return: list of chunks
+        """
+        if len(words) <= seq_size:
+            res = [words + [1 for x in range(seq_size-len(words))]]
+        else:
+            remain = list(words)  # don't operate inplace
+            res = list()
+            while remain:
+                add, remain = remain[:seq_size], remain[seq_size:]
+                if len(add) < seq_size:
+                    add = add + [1 for x in range(seq_size-len(add))]
+                res.append(add)
+        return res
+
+
+
+
---
+++ Model/Scripts/Settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    data_folder = "../Data/"
+    data_path = "../Data/shuffled-full-set-hashed.csv"
+    word_dict = "../Data/WordToIndex.json"
+    seq_size = 200
+    train_path = data_folder + "train.h5"
+    valid_path = data_folder + "valid.h5"
+    test_path = data_folder + "test.h5"
