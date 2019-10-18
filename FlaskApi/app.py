6eea59d8fe8513a62ed2df385b3abc178aa575c6
Chenyu Xi <xi_chenyu@163.com>
2019-10-18 17:10:23 -0400

    add CORS to flask

--- FlaskApi/app.py
+++ FlaskApi/app.py
@@ -1,12 +1,20 @@
 from Evaluate import evaluator
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route('/label/<string:sentence>', methods=['GET'])
-def get_label(sentence):
-    result = evaluator.label_predict(sentence)
+@app.route('/top_labels', methods=['GET'])
+def get_label():
+    try:
+        sentence = request.args.get('document')
+        result = evaluator.label_predict(sentence)
+    except AttributeError:
+        result = {
+            "error": "No Documents"
+        }
     return jsonify(result)
 
 
@@ -18,7 +26,7 @@ if __name__ == '__main__':
     @click.option('--debug', is_flag=True)
     @click.option('--threaded', is_flag=True)
     @click.argument('HOST', default='0.0.0.0')
-    @click.argument('PORT', default=8111, type=int)
+    @click.argument('PORT', default=6516, type=int)  # default GCP port
     def run(debug, threaded, host, port):
         """
         This function handles command line parameters.
